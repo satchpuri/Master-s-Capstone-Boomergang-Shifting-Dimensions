b9bcd37c87ca3d8eeeeda4772bc384d64be940cc
Nitish Victor <nithishvictor@gmail.com>
2018-08-29 15:34:33 -0400

    Basic boomerang

--- Capstone Boomerang/Assets/Scripts/BoomerangManager.cs
+++ Capstone Boomerang/Assets/Scripts/BoomerangManager.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class BoomerangManager : MonoBehaviour
 {
+    enum BoomerangDirection
+    {
+        Front, Back
+    };
 
     public GameObject BoomerangPrefab = null;
-
+    private BoomerangDirection direction;
     private GameObject boomerangInstance = null;
-
     private Vector3 toPosition;
+    private Vector3 positionWhileThrowing;
+
     // Use this for initialization
     void Start()
     {
@@ -21,12 +26,22 @@ public class BoomerangManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            boomerangInstance = Instantiate(BoomerangPrefab, transform.parent, true);
-            toPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(Input.mousePosition);
-            Debug.Log(toPosition);
-        }
+            if (boomerangInstance != null)
+            {
+                Destroy(boomerangInstance);
+            }
 
+            boomerangInstance = Instantiate(BoomerangPrefab, transform.position, transform.rotation);
+            toPosition = new Vector3(4, -1, 0);
+            var mousePos = Input.mousePosition;
+            mousePos.z = -Camera.main.transform.position.z;
+            toPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            toPosition.z = 0;
+            positionWhileThrowing = transform.position;
+            Debug.Log(Input.mousePosition);
+            Debug.Log(Camera.main.ScreenToWorldPoint(mousePos));
+            direction = BoomerangDirection.Front;
+        }
         if (boomerangInstance != null)
         {
             MoveBoomerang();
@@ -35,8 +50,24 @@ public class BoomerangManager : MonoBehaviour
 
     void MoveBoomerang()
     {
-        var direction = (toPosition - transform.position).normalized;
-        direction.z = 0;
-        boomerangInstance.transform.Translate(direction * 5F * Time.deltaTime);
+        if ((boomerangInstance.transform.position - toPosition).magnitude > 0.1f && direction == BoomerangDirection.Front)
+        {
+            var direction = (toPosition - positionWhileThrowing).normalized;
+            boomerangInstance.transform.position = boomerangInstance.transform.position + direction * 5F * Time.deltaTime;
+        }
+        else
+        {
+            direction = BoomerangDirection.Back;
+            if(boomerangInstance.transform.position.x - transform.position.x > 0.1f)
+            {
+                var direction = -(toPosition - positionWhileThrowing).normalized;
+                boomerangInstance.transform.position = boomerangInstance.transform.position + direction * 5F * Time.deltaTime;
+            }
+            else
+            {
+                Destroy(boomerangInstance);
+            }
+        }
+
     }
 }
